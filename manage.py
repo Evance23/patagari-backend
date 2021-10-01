7c95d34fccd92fc3181cf452fd1f64aefd440875
Evance23 <evancebarracks01@gmail.com>
2021-10-01 08:32:25 +0300

    Add new  file changes

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@ import unittest
 
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from app.main import create_app, db
 
