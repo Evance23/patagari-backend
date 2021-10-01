48f18ca439791377097472d89a32d4454412fcac
Evance23 <evancebarracks01@gmail.com>
2021-10-01 06:09:13 +0300

    Add class config

---
+++ app/main/config.py
@@ -0,0 +1,10 @@
+import os
+
+# uncomment the line below for postgres database url from environment variable
+# postgres_local_base = os.environ['DATABASE_URL']
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
+    DEBUG = False
